15268ab4a230fab9c2cdc7515c71e5086939014c
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2019-11-20 12:20:43 +0900

    Initial implementation of Clusterer

---
+++ src/main/java/jp/ac/titech/c/se/stein/core/Graph.java
@@ -0,0 +1,147 @@
+package jp.ac.titech.c.se.stein.core;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.jgrapht.alg.lca.NaiveLCAFinder;
+import org.jgrapht.graph.DirectedAcyclicGraph;
+
+import jp.ac.titech.c.se.stein.core.Graph.Edge;
+import jp.ac.titech.c.se.stein.core.Graph.Vertex;
+
+/**
+ * jGraphT DAG for a commit graph.
+ */
+public class Graph extends DirectedAcyclicGraph<Vertex, Edge> {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The constructor.
+     */
+    public Graph() {
+        super(null, new EdgeSupplier(), false);
+    }
+
+    /**
+     * Checks whether two vertices can be merged (no parent-child relationship).
+     */
+    public boolean isMergeable(final Vertex base, final Vertex target) {
+        final Set<Vertex> lca = new NaiveLCAFinder<Vertex, Edge>(this).getLCASet(base, target);
+        return !lca.contains(base) && !lca.contains(target);
+    }
+
+    /**
+     * Merge the <code>target</code> vertex into <code>base</code> vertex.
+     *
+     * @param base
+     * @param target
+     */
+    public void mergeVertices(final Vertex base, final Vertex target) {
+        if (base.equals(target)) {
+            return;
+        }
+        // assert isMergeable(base, target);
+
+        for (final Edge e : new ArrayList<>(outgoingEdgesOf(target))) {
+            final Vertex v = getEdgeTarget(e);
+            removeEdge(e);
+            // This will use newly created edge having the latest index
+            addEdge(base, v);
+        }
+        for (final Edge e : new ArrayList<>(incomingEdgesOf(target))) {
+            final Vertex v = getEdgeSource(e);
+            removeEdge(e);
+            addEdge(v, base, e); // keep the original edge order
+        }
+        removeVertex(target);
+    }
+
+    /**
+     * Gets the parent vertices of the given vertex.
+     */
+    public List<Vertex> getParents(final Vertex v) {
+        final List<Edge> outgoings = new ArrayList<>(outgoingEdgesOf(v));
+        Collections.sort(outgoings);
+
+        final List<Vertex> result = new ArrayList<>();
+        for (final Edge e : outgoings) {
+            result.add(getEdgeTarget(e));
+        }
+        return result;
+    }
+
+    /**
+     * Vertices of the graph (commits).
+     */
+    public static class Vertex {
+        public final ObjectId id;
+
+        public static Vertex of(final String name) {
+            return of(ObjectId.fromString(name));
+        }
+
+        public static Vertex of(final ObjectId id) {
+            return new Vertex(id);
+        }
+
+        public static Vertex of(final RevCommit commit) {
+            return of(commit.getId());
+        }
+
+        protected Vertex(final ObjectId id) {
+            this.id = id;
+        }
+
+        public boolean is(final String name) {
+            return id.name().equals(name);
+        }
+
+        @Override
+        public int hashCode() {
+            return id.hashCode();
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            return obj instanceof Vertex && id.equals(((Vertex) obj).id);
+        }
+
+        @Override
+        public String toString() {
+            return id.name();
+        }
+    }
+
+    /**
+     * Edges of the graph (parent relationship).
+     */
+    public static class Edge implements Comparable<Edge> {
+        public final int index;
+
+        Edge(final int index) {
+            this.index = index;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return index - o.index;
+        }
+    }
+
+    /**
+     * An edge supplier that gives a sequential number to each edge.
+     */
+    public static class EdgeSupplier implements Supplier<Edge> {
+        private int index = 0;
+
+        @Override
+        public Edge get() {
+            return new Edge(++index);
+        }
+    }
+}
---
+++ src/main/java/jp/ac/titech/c/se/stein/sample/Clusterer.java
@@ -0,0 +1,154 @@
+package jp.ac.titech.c.se.stein.sample;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectInserter;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+
+import jp.ac.titech.c.se.stein.CLI;
+import jp.ac.titech.c.se.stein.core.Config;
+import jp.ac.titech.c.se.stein.core.Configurable;
+import jp.ac.titech.c.se.stein.core.Context;
+import jp.ac.titech.c.se.stein.core.Graph;
+import jp.ac.titech.c.se.stein.core.Graph.Vertex;
+import jp.ac.titech.c.se.stein.core.RepositoryRewriter;
+
+public class Clusterer extends RepositoryRewriter implements Configurable {
+    private static final Logger log = LoggerFactory.getLogger(Clusterer.class);
+
+    private List<List<String>> clusters;
+
+    private final Graph graph = new Graph();
+
+    @Override
+    public void addOptions(final Config conf) {
+        super.addOptions(conf);
+        conf.addOption(null, "clusters", true, "set clustering info");
+    }
+
+    @Override
+    public void configure(final Config conf) {
+        super.configure(conf);
+        if (conf.hasOption("clusters")) {
+            clusters = loadJSON(conf.getOptionValue("clusters"));
+        }
+    }
+
+    /**
+     * Loads the clustering info.
+     */
+    protected List<List<String>> loadJSON(final String filename) {
+        final Gson gson = new Gson();
+        final TypeToken<List<List<String>>> t = new TypeToken<List<List<String>>>() {
+        };
+        try {
+            return gson.fromJson(new FileReader(filename), t.getType());
+        } catch (final JsonIOException | JsonSyntaxException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void rewriteCommits(final Context c) {
+        buildGraph(c);
+        mergeClusters();
+        walkGraph((commit) -> rewriteCommit(commit, c), c);
+    }
+
+    protected void buildGraph(final Context c) {
+        try (final RevWalk walk = prepareRevisionWalk(c)) {
+            for (final RevCommit commit : walk) {
+                final Vertex v = Vertex.of(commit);
+                graph.addVertex(v);
+                for (final RevCommit parent : commit.getParents()) {
+                    final Vertex p = Vertex.of(parent);
+                    graph.addVertex(p);
+                    graph.addEdge(v, p);
+                }
+            }
+        }
+        log.debug("Graph: {} vertices, {} edges", graph.vertexSet().size(), graph.edgeSet().size());
+    }
+
+    protected void walkGraph(final Consumer<RevCommit> f, final Context c) {
+        // TODO: More efficient implementation
+        final List<ObjectId> ids = new ArrayList<>();
+        for (final Vertex v : graph) {
+            ids.add(v.id);
+        }
+
+        try (final ObjectInserter ins = writeRepo.newObjectInserter()) {
+            this.inserter = ins;
+            try (final RevWalk walk = prepareRevisionWalk(c)) {
+                for (int i = ids.size() - 1; i >= 0; i--) {
+                    try {
+                        final RevCommit commit = walk.parseCommit(ids.get(i));
+                        f.accept(commit);
+                    } catch (final IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            this.inserter = null;
+        }
+    }
+
+    protected void mergeClusters() {
+        int merged = 0;
+        for (final List<String> cluster : clusters) {
+            final Vertex base = Vertex.of(cluster.get(0));
+            for (int i = 1; i < cluster.size(); i++) {
+                final Vertex target = Vertex.of(cluster.get(i));
+                if (mergeVertices(graph, base, target)) {
+                    merged++;
+                }
+            }
+        }
+        log.debug("Merged {} commits in total", merged);
+    }
+
+    protected boolean mergeVertices(final Graph graph, final Vertex base, final Vertex target) {
+        if (!graph.isMergeable(base, target)) {
+            log.debug("Cycle detected, avoiding merging commits: {} <- {}", base, target);
+            return false;
+        }
+        log.debug("Merge commits: {} <- {}", base, target);
+        graph.mergeVertices(base, target);
+        return true;
+    }
+
+    @Override
+    protected ObjectId[] rewriteParents(final ObjectId[] parents, final Context c) {
+        final List<Vertex> parentVertices = graph.getParents(Vertex.of(c.getCommit()));
+        final ObjectId[] newParents = new ObjectId[parentVertices.size()];
+        for (int i = 0; i < parentVertices.size(); i++) {
+            newParents[i] = parentVertices.get(i).id;
+        }
+        if (log.isDebugEnabled()) {
+            final String[] before = Arrays.stream(parents).map((id) -> id.name()).toArray(String[]::new);
+            final String[] after = Arrays.stream(newParents).map((id) -> id.name()).toArray(String[]::new);
+            log.debug("Substitute parents: {} -> {} ({})", before, after, c);
+        }
+        return super.rewriteParents(newParents, c);
+    }
+
+    public static void main(final String[] args) {
+        new CLI(Clusterer.class, args).run();
+    }
+}
