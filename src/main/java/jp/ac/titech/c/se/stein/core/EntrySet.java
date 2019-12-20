020fec7fecf518f538737525e1f78d746abfeb4c
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2019-12-20 23:01:28 +0900

    Simplify hashCode and equals

--- src/main/java/jp/ac/titech/c/se/stein/core/EntrySet.java
+++ src/main/java/jp/ac/titech/c/se/stein/core/EntrySet.java
@@ -2,6 +2,7 @@ package jp.ac.titech.c.se.stein.core;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
@@ -26,11 +27,17 @@ public interface EntrySet {
 
         public final String directory;
 
+        public Object data;
+
         public Entry(final FileMode mode, final String name, final ObjectId id, final String directory) {
             this.mode = mode;
             this.name = name;
             this.id = id;
             this.directory = directory;
+        }
+
+        public Entry(final FileMode mode, final String name, final ObjectId id) {
+            this(mode, name, id, null);
         }
 
         public String getPath() {
@@ -57,56 +64,19 @@ public interface EntrySet {
 
         @Override
         public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + (id == null ? 0 : id.hashCode());
-            result = prime * result + (mode == null ? 0 : mode.hashCode());
-            result = prime * result + (name == null ? 0 : name.hashCode());
-            result = prime * result + (directory == null ? 0 : directory.hashCode());
-            return result;
+            return Objects.hash(id, mode, name, directory);
         }
 
         @Override
         public boolean equals(final Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
+            if (!(obj instanceof Entry)) {
                 return false;
             }
             final Entry other = (Entry) obj;
-            if (id == null) {
-                if (other.id != null) {
-                    return false;
-                }
-            } else if (!id.equals(other.id)) {
-                return false;
-            }
-            if (mode == null) {
-                if (other.mode != null) {
-                    return false;
-                }
-            } else if (!mode.equals(other.mode)) {
-                return false;
-            }
-            if (name == null) {
-                if (other.name != null) {
-                    return false;
-                }
-            } else if (!name.equals(other.name)) {
-                return false;
-            }
-            if (directory == null) {
-                if (other.directory != null) {
-                    return false;
-                }
-            } else if (!directory.equals(other.directory)) {
-                return false;
-            }
-            return true;
+            return Objects.equals(id, other.id) &&
+                   Objects.equals(mode, other.mode) &&
+                   Objects.equals(name, other.name) &&
+                   Objects.equals(directory, other.directory);
         }
     }
 
