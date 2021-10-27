8b458414010076e6c4aceb8c6084b78600d8ec1e
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2021-10-27 15:02:59 +0900

    Condition filtering as a decorator

--- src/main/java/jp/ac/titech/c/se/stein/core/Cache.java
+++ src/main/java/jp/ac/titech/c/se/stein/core/Cache.java
@@ -1,42 +1,32 @@
 package jp.ac.titech.c.se.stein.core;
 
 import java.util.AbstractMap;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 
 public class Cache<K, V> extends AbstractMap<K, V> {
-
     private final Map<K, V> frontend, backend;
 
-    private final Predicate<K> condition;
-
-    public Cache(final Map<K, V> frontend, final Map<K, V> backend, final Predicate<K> condition) {
+    public Cache(final Map<K, V> frontend, final Map<K, V> backend) {
         this.frontend = frontend;
         this.backend = backend;
-        this.condition = condition;
+    }
+
+    public Cache(final Map<K, V> frontend, final Map<K, V> backend, final Predicate<K> condition) {
+        this(frontend, new Filter<>(backend, condition));
     }
 
     @Override
     public V get(final Object key) {
-        V result = frontend.get(key);
-        if (result == null && condition.test((K) key)) {
-            result = backend.get(key);
-            if (result != null) {
-                @SuppressWarnings("unchecked")
-                final K k = (K) key;
-                frontend.put(k, result);
-            }
-        }
-        return result;
+        return frontend.computeIfAbsent((K) key, backend::get);
     }
 
     @Override
     public V put(final K key, final V value) {
-        if (condition.test(key)) {
-            backend.put(key, value);
-        }
+        backend.put(key, value);
         return frontend.put(key, value);
     }
 
@@ -52,5 +42,53 @@ public class Cache<K, V> extends AbstractMap<K, V> {
     public void clear() {
         frontend.clear();
         backend.clear();
+    }
+
+    public static class Filter<K, V> extends AbstractMap<K, V> {
+        private final Map<K, V> delegatee;
+        private final Predicate<K> condition;
+
+        public Filter(final Map<K, V> delegatee, final Predicate<K> condition) {
+            this.delegatee = delegatee;
+            this.condition = condition;
+        }
+
+        @Override
+        public V get(final Object key) {
+            return condition.test((K) key) ? delegatee.get(key) : null;
+        }
+
+        @Override
+        public V put(final K key, final V value) {
+            return condition.test((K) key) ? delegatee.put(key, value) : value;
+        }
+
+        @Override
+        public Set<Entry<K, V>> entrySet() {
+            return delegatee.entrySet();
+        }
+    }
+
+    public static class PutOnly<K, V> extends AbstractMap<K, V> {
+        private final Map<K, V> delegatee;
+
+        public PutOnly(final Map<K, V> delegatee) {
+            this.delegatee = delegatee;
+        }
+
+        @Override
+        public V get(final Object key) {
+            return null;
+        }
+
+        @Override
+        public V put(final K key, final V value) {
+            return delegatee.put(key, value);
+        }
+
+        @Override
+        public Set<Entry<K, V>> entrySet() {
+            return Collections.emptySet();
+        }
     }
 }
