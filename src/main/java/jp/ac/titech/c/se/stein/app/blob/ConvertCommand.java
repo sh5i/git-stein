03f75e8fd94f83b5d4194d3af9fb55e2fee4fe9a
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2023-08-08 20:24:09 +0900

    Split Convert app into ConvertCommand and ConvertHttp

---
+++ src/main/java/jp/ac/titech/c/se/stein/app/blob/ConvertCommand.java
@@ -0,0 +1,57 @@
+package jp.ac.titech.c.se.stein.app.blob;
+
+import jp.ac.titech.c.se.stein.core.Context;
+import jp.ac.titech.c.se.stein.core.HotEntry;
+import jp.ac.titech.c.se.stein.rewriter.BlobTranslator;
+import jp.ac.titech.c.se.stein.rewriter.NameFilter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Mixin;
+import picocli.CommandLine.Option;
+
+import java.io.*;
+
+@Slf4j
+@ToString
+@Command(name = "@convert-cmd", description = "Convert files via command execution")
+public class ConvertCommand implements BlobTranslator {
+    @Option(names = "--cmd", split = " ", paramLabel = "<cmdline>", description = "Command with arguments",
+            required = true)
+    protected String[] cmdline;
+
+    @Mixin
+    private final NameFilter filter = new NameFilter();
+
+    @Override
+    public HotEntry rewriteBlobEntry(final HotEntry.SingleHotEntry entry, final Context c) {
+        if (filter.accept(new File(entry.getName()))) {
+            final byte[] blob = entry.getBlob();
+            final byte[] converted = convert(blob, c);
+            return entry.update(converted);
+        }
+        return entry;
+    }
+
+    protected byte[] convert(final byte[] content, final Context c) {
+        try {
+            final Process proc = new ProcessBuilder()
+                    .command(cmdline)
+                    .redirectError(ProcessBuilder.Redirect.INHERIT)
+                    .start();
+            try (final OutputStream out = proc.getOutputStream()) {
+                out.write(content);
+            }
+            try (final InputStream in = proc.getInputStream()) {
+                return in.readAllBytes();
+            } finally {
+                try (final BufferedReader err = new BufferedReader(new InputStreamReader(proc.getErrorStream()))) {
+                    err.lines().forEach(line -> log.warn("stderr: {} {}", line, c));
+                }
+            }
+        } catch (final IOException e) {
+            log.error(e.getMessage(), e);
+            return content;
+        }
+    }
+}
---
+++ src/main/java/jp/ac/titech/c/se/stein/app/blob/ConvertHttp.java
@@ -0,0 +1,61 @@
+package jp.ac.titech.c.se.stein.app.blob;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import jp.ac.titech.c.se.stein.core.HotEntry;
+import jp.ac.titech.c.se.stein.rewriter.BlobTranslator;
+import jp.ac.titech.c.se.stein.rewriter.NameFilter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+
+import jp.ac.titech.c.se.stein.core.Context;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Mixin;
+
+@Slf4j
+@ToString
+@Command(name = "@convert-http", description = "Convert files via external HTTP API")
+public class ConvertHttp implements BlobTranslator {
+    @Option(names = "--endpoint", paramLabel = "<url>", description = "HTTP Web API endpoint",
+            required = true)
+    protected URL endpoint;
+
+    @Mixin
+    private final NameFilter filter = new NameFilter();
+
+    @Override
+    public HotEntry rewriteBlobEntry(final HotEntry.SingleHotEntry entry, final Context c) {
+        if (filter.accept(new File(entry.getName()))) {
+            final byte[] blob = entry.getBlob();
+            final byte[] converted = convert(entry.getName(), blob, c);
+            return entry.update(converted);
+        }
+        return entry;
+    }
+
+    protected byte[] convert(final String filename, final byte[] content, final Context c) {
+        try {
+            final HttpURLConnection conn = (HttpURLConnection) endpoint.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setAllowUserInteraction(false);
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-type", "text/plain");
+            conn.setRequestProperty("Accept", "text/plain");
+            conn.setRequestProperty("Content-Length", String.valueOf(content.length));
+            conn.setRequestProperty("X-Filename", filename);
+            conn.getOutputStream().write(content);
+            if (conn.getResponseCode() == 200) {
+                return IOUtils.toByteArray(conn.getInputStream());
+            } else {
+                log.error("Bad status code in response: {} {}", conn.getResponseCode(), c);
+            }
+        } catch (final IOException e) {
+            log.error(e.getMessage(), e);
+        }
+        return content;
+    }
+}
